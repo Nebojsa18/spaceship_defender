a47a00b5c7224a10afead7a579638d793c07e795
Nebojsa18 <nebojsa.radojkovic2000@gmail.com>
2023-05-29 01:29:10 +0200

    Score and enemy projectiles added

--- Spaceship Defender/Assets/Scripts/Projectile.cs
+++ Spaceship Defender/Assets/Scripts/Projectile.cs
@@ -6,10 +6,12 @@ public class Projectile : MonoBehaviour
 {
     public float moveSpeed;
     public GameObject explosionPrefab;
+    private PointManager pointManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pointManager = GameObject.Find("PointManager").GetComponent<PointManager>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,7 @@ public class Projectile : MonoBehaviour
         if(collision.gameObject.tag =="Enemy"){
             Instantiate(explosionPrefab,transform.position,Quaternion.identity);
             Destroy(collision.gameObject);
+            pointManager.UpdateScore(50);
             Destroy(gameObject);
 
         }
